fe9d01a9480ba546372c8743135405cfa83b82ed
Hanadoggy <78478981+Hanadoggy@users.noreply.github.com>
2023-06-16 15:47:22 +0900

    Initial commit

---
+++ src/test/java/dbwls/springThymeleafBasic/SpringThymeleafBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package dbwls.springThymeleafBasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringThymeleafBasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
